b2f4b61fda84baa5de0b6123c3a25c2f51a23621
TCSE <talik@tcse-cms.com>
2020-08-31 19:50:49 +0300

    версия 1.0.5 с описанием на странице справочной инфы модуля.

--- engine/modules/mod_tcse/autorss_pro/admin/footer.php
+++ engine/modules/mod_tcse/autorss_pro/admin/footer.php
@@ -10,7 +10,7 @@ if (!defined('DATALIFEENGINE') || !defined('LOGGED_IN')) {
 	<div class="panel-content">
 		<div class="panel-body">
 			&copy; 2019 Модуль <b>Auto RSS Pro by TCSE-cms.com</b> — Автоматическая публикация новостей из RSS каналов.<br>
-			Версия <b>1.0.3</b> от <b>2019-01-17</b><br>
+			Версия <b>1.0.5</b> от <b>2020-08-31</b><br>
 			Разработано в <a href="http://tcse-cms.com" target="_blank">TCSE-cms.com</a>
 		</div>
 	</div>
